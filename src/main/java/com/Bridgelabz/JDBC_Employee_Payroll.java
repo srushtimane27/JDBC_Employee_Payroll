d34fb6bf31e7592da26dc85818e0df1ab54b5a33
srushtimane27 <srushtimane31@gmail.com>
2022-08-19 19:54:24 +0530

    Employee payroll service to retrive the employee payroll from database

--- src/main/java/com/Bridgelabz/JDBC_Employee_Payroll.java
+++ src/main/java/com/Bridgelabz/JDBC_Employee_Payroll.java
@@ -1,14 +1,19 @@
 package com.Bridgelabz;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+
+import com.sun.xml.internal.fastinfoset.util.CharArray;
+import javafx.util.converter.CharacterStringConverter;
+
+import java.sql.*;
+import java.util.Scanner;
 
 public class JDBC_Employee_Payroll {
     public static void main(String[] args) {
         Connection con = null;
-        Statement stm = null;
+        PreparedStatement stm = null;
+
+        String query = "select * from payroll_service.employee";
+        Scanner sc = new Scanner(System.in);
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("driver register");
@@ -16,8 +21,31 @@ public class JDBC_Employee_Payroll {
             con = DriverManager.getConnection("jdbc:mysql://localhost:3307?user=root&password=Srushti@2712");
             System.out.println("connection establish");
 
-            stm = con.createStatement();
+            stm = con.prepareStatement(query);
             System.out.println("platform created");
+
+            System.out.println("Please insert the id to fetch the particular record");
+            int userId = sc.nextInt();
+
+
+            ResultSet rs = stm.executeQuery();
+
+            if(rs.next()){
+                int id = rs.getInt("id");
+                int company_id = rs.getInt(2);
+                String name = rs.getString(3);
+                int phone_number = rs.getInt(4);
+                String address = rs.getString(5);
+                String gender = rs.getString(6);
+                Date start = rs.getDate(7);
+
+                System.out.println("User id -> "+id+ "Company_id -> "+company_id+ "User name -> "+name+ "User phone_number -> "+phone_number+ "User address -> "+address+ "User gender -> "+gender+ "User date -> "+start);
+            }else{
+                System.out.println("No record found with userId: " +userId);
+            }
+
+            //     System.out.println("Data inserted");
+
         }catch (Exception e){
             System.out.println(e.getMessage());
         }finally {
@@ -28,9 +56,16 @@ public class JDBC_Employee_Payroll {
                 {
                     System.out.println(s.getMessage());
                 }
+            }if (stm != null){
+                try {
+                    stm.close();
+                }catch (SQLException s){
+                    System.out.println(s.getMessage());
+
+                }if (sc != null){
+                    sc.close();
+                }
             }
         }
     }
 }
-
-
