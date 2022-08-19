648c0dfce014309f665c7d96b317b5dcc0a60163
srushtimane27 <srushtimane31@gmail.com>
2022-08-19 20:24:51 +0530

    Update salary of Terisa

--- src/main/java/com/Bridgelabz/JDBC_Employee_Payroll.java
+++ src/main/java/com/Bridgelabz/JDBC_Employee_Payroll.java
@@ -1,33 +1,28 @@
 package com.Bridgelabz;
 
-import java.sql.*;
-import java.util.Scanner;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JDBC_Employee_Payroll {
     public static void main(String[] args) {
         Connection con = null;
-        PreparedStatement stm = null;
-        String query = "update payroll_service.payroll set basic_pay = '3000000.00' where id = 2";
+        Statement stm = null;
 
-        Scanner sc = new Scanner(System.in);
+        String query = "update payroll_service.payroll set basic_pay = '300' where id = 2";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("driver register");
 
             con = DriverManager.getConnection("jdbc:mysql://localhost:3307?user=root&password=Srushti@2712");
-
             System.out.println("connection establish");
 
-            stm = con.prepareStatement(query);
-            //   System.out.println("platform created");
+            stm = con.createStatement();
+            System.out.println("platform created");
 
-
-            stm.executeUpdate();
-
-            System.out.println("Data inserted");
-
-            //     System.out.println("Data inserted");
-
+            stm.executeUpdate(query);
+            System.out.println("Data Updated");
         }catch (Exception e){
             System.out.println(e.getMessage());
         }finally {
@@ -43,9 +38,6 @@ public class JDBC_Employee_Payroll {
                     stm.close();
                 }catch (SQLException s){
                     System.out.println(s.getMessage());
-
-                }if (sc != null){
-                    sc.close();
                 }
             }
         }
