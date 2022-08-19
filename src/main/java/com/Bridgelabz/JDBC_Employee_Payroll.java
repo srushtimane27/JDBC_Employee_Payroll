194ca52cfe43ff6494cf4cfc0c9b577f7eac7a0a
srushtimane27 <srushtimane31@gmail.com>
2022-08-19 20:10:19 +0530

    Update the base pay of employee Terisa

--- src/main/java/com/Bridgelabz/JDBC_Employee_Payroll.java
+++ src/main/java/com/Bridgelabz/JDBC_Employee_Payroll.java
@@ -1,9 +1,5 @@
 package com.Bridgelabz;
 
-
-import com.sun.xml.internal.fastinfoset.util.CharArray;
-import javafx.util.converter.CharacterStringConverter;
-
 import java.sql.*;
 import java.util.Scanner;
 
@@ -11,38 +7,24 @@ public class JDBC_Employee_Payroll {
     public static void main(String[] args) {
         Connection con = null;
         PreparedStatement stm = null;
+        String query = "update payroll_service.payroll set basic_pay = '3000000.00' where id = 2";
 
-        String query = "select * from payroll_service.employee";
         Scanner sc = new Scanner(System.in);
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("driver register");
 
             con = DriverManager.getConnection("jdbc:mysql://localhost:3307?user=root&password=Srushti@2712");
+
             System.out.println("connection establish");
 
             stm = con.prepareStatement(query);
-            System.out.println("platform created");
-
-            System.out.println("Please insert the id to fetch the particular record");
-            int userId = sc.nextInt();
+            //   System.out.println("platform created");
 
 
-            ResultSet rs = stm.executeQuery();
+            stm.executeUpdate();
 
-            if(rs.next()){
-                int id = rs.getInt("id");
-                int company_id = rs.getInt(2);
-                String name = rs.getString(3);
-                int phone_number = rs.getInt(4);
-                String address = rs.getString(5);
-                String gender = rs.getString(6);
-                Date start = rs.getDate(7);
-
-                System.out.println("User id -> "+id+ "Company_id -> "+company_id+ "User name -> "+name+ "User phone_number -> "+phone_number+ "User address -> "+address+ "User gender -> "+gender+ "User date -> "+start);
-            }else{
-                System.out.println("No record found with userId: " +userId);
-            }
+            System.out.println("Data inserted");
 
             //     System.out.println("Data inserted");
 
