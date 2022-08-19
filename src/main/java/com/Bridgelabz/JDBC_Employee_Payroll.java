0cd9211926c3a5a5b41157e197498173ae02f339
srushtimane27 <srushtimane31@gmail.com>
2022-08-19 19:11:32 +0530

    Java program connect to employee_payroll

--- src/main/java/com/Bridgelabz/JDBC_Employee_Payroll.java
+++ src/main/java/com/Bridgelabz/JDBC_Employee_Payroll.java
@@ -1,2 +1,36 @@
-package com.Bridgelabz;public class JDBC_Employee_Payroll {
+package com.Bridgelabz;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBC_Employee_Payroll {
+    public static void main(String[] args) {
+        Connection con = null;
+        Statement stm = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("driver register");
+
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3307?user=root&password=Srushti@2712");
+            System.out.println("connection establish");
+
+            stm = con.createStatement();
+            System.out.println("platform created");
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }finally {
+            if (con != null){
+                try{
+                    con.close();
+                }catch(SQLException s)
+                {
+                    System.out.println(s.getMessage());
+                }
+            }
+        }
+    }
 }
+
+
