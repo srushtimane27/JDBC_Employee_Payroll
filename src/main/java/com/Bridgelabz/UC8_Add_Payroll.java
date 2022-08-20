0684350a556648ea3745e764495b23adf741adb7
srushtimane27 <srushtimane31@gmail.com>
2022-08-20 23:47:10 +0530

    Add to payroll details of new employee

---
+++ src/main/java/com/Bridgelabz/UC8_Add_Payroll.java
@@ -0,0 +1,52 @@
+package com.Bridgelabz;
+
+import java.sql.*;
+
+public class UC8_Add_Payroll {
+    public static void main(String[] args) {
+        Connection con = null;
+        PreparedStatement stm = null;
+        String query = "insert into payroll_service.payroll values(?,?,?,?,?,?,?)";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("driver register");
+
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3307?user=root&password=Srushti@2712");
+            System.out.println("connection establish");
+
+            stm = con.prepareStatement(query);
+            System.out.println("platform created");
+
+            stm.setInt(1, 4);
+            stm.setString(2, "Akshay");
+            stm.setDouble(3,4000000);
+            stm.setDouble(4,1000000);
+            stm.setDouble(5,1000000);
+            stm.setDouble(6,1000000);
+            stm.setDouble(7,1000000);
+            stm.executeUpdate();
+
+            System.out.println("Data inserted");
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }finally {
+            if (con != null){
+                try{
+                    con.close();
+                }catch(SQLException s)
+                {
+                    System.out.println(s.getMessage());
+                }
+            }if (stm != null){
+                try {
+                    stm.close();
+                }catch (SQLException s){
+                    System.out.println(s.getMessage());
+                }
+            }
+        }
+    }
+}
+
