2e5866e7c998fb5e2c5532c5298ef32dad94281b
srushtimane27 <srushtimane31@gmail.com>
2022-08-21 00:16:12 +0530

    Delete and add employee to payroll database

--- src/main/java/com/Bridgelabz/Refactor_UC8_Delete_Add_Employee.java
+++ src/main/java/com/Bridgelabz/Refactor_UC8_Delete_Add_Employee.java
@@ -5,12 +5,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class UC12_Remove_Employee {
+public class Refactor_UC8_Delete_Add_Employee {
     public static void main(String[] args) {
         Connection con = null;
         Statement stm = null;
 
-        String query = "delete from payroll_service.payroll where id = 4";
+        String query = "delete from payroll_service.payroll where id = 5";
+        String query1 = "insert into payroll_service.payroll values('8', 'Sahil', 6000000, 1000000, 1000000, 1000000, 3000000)";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("driver register");
@@ -18,10 +19,10 @@ public class UC12_Remove_Employee {
             System.out.println("connection establish");
             stm = con.createStatement();
             System.out.println("platform created");
-            //      stm.executeUpdate(query);
-            //      System.out.println("Data inserted");
             stm.executeUpdate(query);
             System.out.println("Data deleted");
+            stm.executeUpdate(query1);
+            System.out.println("Data inserted");
         }catch (Exception e){
             System.out.println(e.getMessage());
         }finally {
@@ -42,3 +43,4 @@ public class UC12_Remove_Employee {
         }
     }
 }
+
